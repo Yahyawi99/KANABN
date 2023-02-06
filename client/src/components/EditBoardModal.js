fe6a9492812063a46d5742b7dd42fd5a0258ffad
Yahyawi99 <yassinyahaywi26@gmail.com>
2023-02-06 21:58:42 +0100

    working on creating new board

--- client/src/components/EditBoardModal.js
+++ client/src/components/EditBoardModal.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useGlobal } from "../context";
 
 const EditBoardModal = () => {
-  const { modalNameToActivate } = useGlobal();
+  const { modalNameToActivate, currentData } = useGlobal();
 
   return (
     <section className="editBoardContainer">
@@ -11,48 +11,36 @@ const EditBoardModal = () => {
       <form noValidate>
         <div>
           <label htmlFor="boardName">Name</label>
-          <input type="text" id="boardName" />
+          <input type="text" id="boardName" value={currentData.name} />
         </div>
 
         <div>
           <label htmlFor="columns">Columns</label>
 
           <div id="columns">
-            <div>
-              <input type="text" />
-              <i>
-                <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="#828FA3" fillRule="evenodd">
-                    <path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z" />
-                    <path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z" />
-                  </g>
-                </svg>
-              </i>
-            </div>
+            {currentData.columns.map((column, index) => {
+              const { _id, name } = column;
 
-            <div>
-              <input type="text" />
-              <i>
-                <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="#828FA3" fillRule="evenodd">
-                    <path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z" />
-                    <path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z" />
-                  </g>
-                </svg>
-              </i>
-            </div>
-
-            <div>
-              <input type="text" />
-              <i>
-                <svg width="15" height="15" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="#828FA3" fillRule="evenodd">
-                    <path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z" />
-                    <path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z" />
-                  </g>
-                </svg>
-              </i>
-            </div>
+              return (
+                <div key={_id}>
+                  <input type="text" value={name} />
+                  {[...currentData.columns].length > 1 && (
+                    <i>
+                      <svg
+                        width="15"
+                        height="15"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <g fill="#828FA3" fillRule="evenodd">
+                          <path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z" />
+                          <path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z" />
+                        </g>
+                      </svg>
+                    </i>
+                  )}
+                </div>
+              );
+            })}
           </div>
 
           <button>+ New Column</button>
